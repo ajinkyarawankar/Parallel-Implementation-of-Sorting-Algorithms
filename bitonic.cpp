bb08fded8cfd9c32896970124e8fc4b26034d5db
Amrit Kataria <amrit.kataria@students.iiit.ac.in>
2018-11-09 01:12:58 +0530

    oddeven bitonic complete

---
+++ bitonic.cpp
@@ -0,0 +1,197 @@
+#include<iostream>                                                                             
+#include<omp.h>           
+#include<time.h>
+#include<cstdlib>
+#include <algorithm>
+using namespace std;
+
+int numThreads = 4;
+int lenArr;
+
+void Swap(int i, int j, int *a, int dir)                                         
+{	
+	if (dir==(a[i]>a[j]))
+    {
+    	int t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+    }
+}
+
+void bitonicMerge(int startIndex, int lastIndex, int dir, int *ar)    
+{
+		int counter = 0;                                                                   
+		int noOfElements = lastIndex - startIndex + 1;
+		for (int j = noOfElements / 2; j > 0; j = j / 2)
+		{
+			counter = 0;
+			for (int i = startIndex; i + j <= lastIndex; i++)
+			{
+					Swap(i, i + j, ar,dir);
+					counter++;
+			}
+		}
+}
+void bitonicSortParallel(int startIndex, int lastIndex, int *ar)                         
+{
+	int noOfElements = lastIndex - startIndex + 1;
+	for (int j = 2; j <= noOfElements; j = j * 2)
+	{
+		#pragma omp parallel for                                                      
+		for (int i = 0; i < noOfElements; i = i + j)
+		{
+			if (((i / j) % 2) == 0)                                                              
+			{
+				bitonicMerge(i, i + j - 1, 1, ar);
+			}
+			else
+			{
+				bitonicMerge(i, i + j - 1, 0, ar);
+			}
+		}
+	}
+}
+
+void bitonicSortSerial(int startIndex, int lastIndex, int *ar)                        
+{
+	int noOfElements = lastIndex - startIndex + 1;
+	for (int j = 2; j <= noOfElements; j = j * 2)
+	{
+		for (int i = 0; i < noOfElements; i = i + j)
+		{
+			if (((i / j) % 2) == 0)                                                              
+			{
+				bitonicMerge(i, i + j - 1, 1, ar);
+			}
+			else
+			{
+				bitonicMerge(i, i + j - 1, 0, ar);
+			}
+		}
+	}
+}
+
+bool checkResult(int *A,int *B,int lenArr){
+	int i = 0;
+	while(i<lenArr)
+	{
+		if(A[i]!=B[i]) { return false; }
+		i++;
+	}
+	return true;
+}
+
+void printArray(int *A,int lenArr){
+	if( lenArr <= 30 ){
+		for(int i = 0 ; i < lenArr; i++ ) 
+		{
+			printf("%d ", A[i]);
+		}
+		printf("\n");
+	}
+}
+
+int* addPadding(int *A){
+	int lenArr2 = 1;
+	while(lenArr2 < lenArr)
+	    lenArr2*=2;
+	int *B = new int[lenArr2];
+	int i=0;
+	while(i<lenArr)
+	{
+		B[i]=A[i];
+		i++;
+	}
+	while(i<lenArr2)
+	{
+		B[i]=0;
+		i++;
+	}
+	lenArr=lenArr2;
+	return B;
+}
+
+int* removePadding(int *A,int originalLength){
+	int r = lenArr-originalLength;
+	int *B = new int[originalLength];
+	int i=0;
+	while(r<lenArr){
+		B[i]=A[r];
+		r++;
+		i++;
+	}
+	return B;
+}
+
+int main()                                                                                      
+{
+	omp_set_num_threads(numThreads);
+	cout<<"Number of Threads are-"<<numThreads<<endl;	                                                   
+	cout << "enter the number of elements to be sorted (number should be in the order of 2^n)";    
+	cin >> lenArr;
+	double startTime, stopTime;
+	int *A = new int[lenArr];
+	int *B = new int[lenArr];
+	int *C = new int[lenArr];
+	srand(time(NULL));
+	int i;
+	printf("Initializing the arrays with random numbers...\n");
+	for (i=0; i<lenArr; i++){
+		A[i] = (rand()%1000000);
+		B[i] = A[i];
+		C[i] = A[i];
+	}
+	printf("Initialization complete\n");
+	printArray(A,lenArr);
+	int originalLength = lenArr;
+	A=addPadding(A);
+	lenArr=originalLength;
+	B=addPadding(B);
+	lenArr=originalLength;
+	C=addPadding(C);
+	
+	printf("\nSorting with serial sort function of 'algorithm.h' ...");
+	startTime = clock();
+	sort(A,A+lenArr);
+	stopTime = clock();
+	A=removePadding(A,originalLength);
+	printArray(A,originalLength);
+	printf("Sorted in (aprox.): %f seconds \n\n", (double)(stopTime-startTime)/CLOCKS_PER_SEC);
+
+	printf("\nSorting with custom serial BitonicSortSerial...");
+	startTime = clock();
+	bitonicSortSerial(0,lenArr-1,B);
+	stopTime = clock();
+	B=removePadding(B,originalLength);
+	printArray(B,originalLength);
+	printf("Sorted in (aprox.): %f seconds \n\n", (double)(stopTime-startTime)/CLOCKS_PER_SEC);
+
+	printf("\nSorting with custom PARALLEL BitonicSortParallel... "); fflush(stdout);
+	startTime = clock();
+	bitonicSortParallel(0,lenArr-1,C);
+	stopTime = clock();
+	C=removePadding(C,originalLength);
+	printArray(C,originalLength);
+	printf("Sorted in (aprox.): %f seconds \n\n", (stopTime-startTime)/CLOCKS_PER_SEC);
+
+	printf("\nChecking if the results are correct...\n");
+	bool correctResult;
+	correctResult = checkResult(A,B,originalLength);
+	if(correctResult==true){
+		printf("The result with 'custom SERIAL BitonicSortSerial' is CORRECT\n");
+	}else{
+		printf("The result with 'custom SERIAL BitonicSortSerial' is INCORRECT!!\n");
+	}
+
+	correctResult = checkResult(A,C,originalLength);
+	if(correctResult==true){
+		printf("The result with 'custom PARALLEL BitonicSortParallel' is CORRECT\n\n");
+	}else{
+		printf("The result with 'custom PARALLEL BitonicSortParallel' is INCORRECT!!\n");
+	}
+
+	free(A);
+	free(C);
+	free(B);
+}
+
---
+++ oddeven.cpp
@@ -0,0 +1,220 @@
+#include <iostream>
+#include <time.h>
+#include <stdlib.h>
+#include<algorithm>
+#include<omp.h>
+#include<vector>
+using namespace std;
+
+int numThreads = 4;
+vector<int> a;
+vector<int> b;
+vector<int> c;
+int lenArr;
+
+void oddEvenMerge(int lo, int n, int r)
+{
+    int m=r*2;
+    if (m<n)
+    {
+        oddEvenMerge(lo, n, m);
+        oddEvenMerge(lo+r, n, m);
+        for (int i=lo+r; i+r<lo+n; i+=m)
+        {
+        	if (a[i]>a[i+r])
+            {
+            	int t=a[i];
+		        a[i]=a[i+r];
+		        a[i+r]=t;
+            }
+        }
+    }
+    else
+    	if (a[lo]>a[lo+r])
+           {
+           	int t=a[lo];
+		    a[lo]=a[lo+r];
+		    a[lo+r]=t;
+        }
+}
+
+void oddEvenMergeSort(int lo, int n)
+{
+    if (n>1)
+    {
+        int m=n/2;
+        oddEvenMergeSort(lo, m);
+        oddEvenMergeSort(lo+m, m);
+        oddEvenMerge(lo, n, 1);
+    }
+}
+
+void sortSerial()
+{
+    oddEvenMergeSort(0, a.size());
+}
+
+void sortParallel(int t)
+{
+	if(t==2){
+		 #pragma omp parallel sections num_threads(2)
+         {
+         	#pragma omp section
+        		oddEvenMergeSort(0, a.size()/2);
+        	 #pragma omp section
+                oddEvenMergeSort(a.size()/2, a.size()/2);
+
+         }
+        oddEvenMerge(0, a.size(), 1);
+	}
+	else if(t==4){
+		#pragma omp parallel sections num_threads(4)
+         {
+         	#pragma omp section
+        		oddEvenMergeSort(0, a.size()/4);
+        	 #pragma omp section
+                oddEvenMergeSort(a.size()/4, a.size()/4);
+			#pragma omp section
+        		oddEvenMergeSort(a.size()/2, a.size()/4);
+        	 #pragma omp section
+                oddEvenMergeSort(a.size()/2 + a.size()/4, a.size()/4);
+         }
+         #pragma omp parallel sections num_threads(2)
+		 {
+         	#pragma omp section
+         	oddEvenMerge(0,a.size()/2,1);
+         	#pragma omp section
+         	oddEvenMerge(a.size()/2,a.size()/2,1);
+		 }
+        oddEvenMerge(0, a.size(), 1);
+	}
+	else{
+		oddEvenMergeSort(0,a.size());
+	}
+}
+
+bool checkResult(vector<int> &A,vector<int> &B){
+	int i = 0;
+	int lenArr=A.size();
+	while(i<lenArr)
+	{
+		if(A[i]!=B[i]) { return false; }
+		i++;
+	}
+	return true;
+}
+
+void printArray(vector<int> &A){
+	int lenArr = A.size();
+	if( lenArr <= 30 ){
+		for(int i = 0 ; i < lenArr; i++ ) 
+		{
+			printf("%d ", A[i]);
+		}
+		printf("\n");
+	}
+}
+
+vector<int> addPadding(vector<int> &A){
+	int lenArr2 = 1;
+	while(lenArr2 < lenArr)
+		lenArr2*=2;
+	vector<int> B(lenArr2);
+	int i=0;
+	while(i<lenArr)
+	{
+		B[i]=A[i];
+		i++;
+	}
+	while(i<lenArr2)
+	{
+		B[i]=0;
+		i++;
+	}
+	lenArr=lenArr2;
+	return B;
+}
+
+vector<int> removePadding(vector<int> &A,int originalLength){
+	int r = lenArr-originalLength;
+	vector<int> B(originalLength);
+	int i=0;
+	while(r<lenArr){
+		B[i]=A[r];
+		r++;
+		i++;
+	}
+	return B;
+}
+
+int main()
+{	
+	double startTime,stopTime;
+	vector<int> temp;
+	srand(time(NULL));
+	cout<<"Number of Threads are-"<<numThreads<<endl;
+    cout<<"enter the number of elements to be sorted (number should be in the order of 2^n)";     
+    cin>>lenArr;
+	int originalLength=lenArr;
+	printf("Initializing the arrays with random numbers...\n");
+	for (int i = 0; i < lenArr; i++){
+		a.push_back((rand() % 50000));
+		b.push_back(a[i]);
+		c.push_back(a[i]);
+	}
+	
+	printf("Initialization complete\n");
+	printf("\n\nArray BEFORE sorting: \n");
+	printArray(a);
+	
+	a=addPadding(a);
+	lenArr=originalLength;
+	b=addPadding(b);
+	lenArr=originalLength;
+	c=addPadding(c);
+	
+	printf("\nSorting with serial sort function of 'algorithm.h' ...");
+	startTime = clock();
+	sort(c.begin(),c.end());
+	stopTime = clock();
+	c=removePadding(c,originalLength);
+	printArray(c);
+	printf("Sorted in (aprox.): %f seconds \n\n", (double)(stopTime-startTime)/CLOCKS_PER_SEC);
+	
+    printf("\nSorting with custom serial OddEvenMergeSortSerial...");	
+	startTime = clock();
+	sortSerial();
+	stopTime = clock();
+	a=removePadding(a,originalLength);
+	printArray(a);
+    printf("Sorted in (aprox.): %f seconds \n",(double)(stopTime-startTime)/CLOCKS_PER_SEC);
+    
+	printf("\nSorting with custom parallel OddEvenMergeSortParallel...");	
+	startTime = clock();
+	temp=a;
+	a=b;
+	sortParallel(numThreads);
+	stopTime = clock();
+	a=removePadding(a,originalLength);
+	printArray(a);
+	b=a;
+	a=temp;
+    printf("Sorted in (approx.): %f seconds \n",(double)(stopTime-startTime)/CLOCKS_PER_SEC);
+
+	printf("\nChecking if the results are correct...\n");
+	bool correctResult = checkResult(a,c);
+	if(correctResult){
+		printf("The result with 'custom SERIAL OddEvenMergeSortSerial' is CORRECT\n");
+	}else{
+		printf("The result with 'custom SERIAL OddEvenMergeSortSerial' is INCORRECT!!\n");
+	}
+
+	correctResult = checkResult(b,c);
+	if(correctResult==true){
+		printf("The result with 'custom PARALLEL OddEvenMergeSortParallel' is CORRECT\n\n");
+	}else{
+		printf("The result with 'custom PARALLEL OddEvenMergeSortParallel' is INCORRECT!!\n");
+	}
+	
+	return 0;
+}
